92c1e90e4284f550c771b7ee2f05b31f2b12cb21
hongteng.pang <hongteng.pang@momenta.ai>
2025-04-03 01:51:45 +0800

    update

--- install.sh
+++ install.sh
@@ -4,40 +4,36 @@
 # @Last Modified time: 2017-12-04 00:07:25
 #!/bin/bash 
 
-apt update
-# 安装最新版的php仓库地址
-apt -y install python-software-properties software-properties-common
-LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-apt update
-apt -y upgrade
 
-# build-essential libtool编译程序所需要的包,包含常用的一些编译组件 libevent-dev是编译memcache时需要的 
-apt -y install git wget vim tmux mosh zsh build-essential libevent-dev libtool
-# nginx 所用到的组件
-apt -y install libpcre3 libpcre3-dev openssl libssl-dev zlib1g-dev libpng-dev
+# 在debian系统下，初始化服务器
+
+
+# 第一步
+apt update
+
+# 安装tmux git wget vim tmux mosh zsh
+apt -y install tmux git wget vim tmux mosh zsh
+wait
+
 # 安装oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" &
 wait
+
 # 获取tmux的配置文件，并替换
 wget -O .tmux.conf https://raw.githubusercontent.com/Xiaolang0/config_files/master/tmux.conf
 mv .tmux.conf ~/
 
-# 根据输入的版本号安装php
-echo " 输入准备安装的PHP版本号: "
-read php_version
-apt -y install php${php_version}-cgi php${php_version}-cli php${php_version}-bcmath php${php_version}-common php${php_version} php${php_version}-curl php${php_version}-fpm php${php_version}-gd php${php_version}-imap php${php_version}-json php${php_version}-mbstring php${php_version}-mcrypt php${php_version}-odbc php${php_version}-opcache php${php_version}-pgsql php${php_version}-readline php${php_version}-xml php${php_version}-xmlrpc php${php_version}-zip
+# 安装docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+sudo echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# 安装memcache
-read -p "是否需要安装最新版的memcached: [y/n]" is_install_memcached
-if [[ $is_install_memcached == y ]]; then
-    wget -O memcached-download http://memcached.org/latest
-    tar -zxvf memcached-download
-    cd memcached-*
-    ./configure --prefix=/usr/local/memcached
-    make && make test && sudo make install
-    echo memcached 安装成功
-
-else
-    echo "不安装memcache，跳过"
-fi
